3746c76ddb7fa5400f2e835ed0d707ae328e032d
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2022-10-05 03:06:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = function (filter) {
+    if (filter === "all") {
+      setMenuItems(items);
+      return;
+    }
+
+    let newItems = items.filter((item) => item.category === filter);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <div className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </div>
+    </main>
+  );
+}
+
+export default App;
